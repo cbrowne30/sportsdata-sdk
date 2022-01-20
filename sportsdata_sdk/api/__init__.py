6a8735625c4ac31d97022526cc527b8c3029dc60
cbrowne30 <chrisbrowne@comcast.net>
2022-01-20 14:58:32 -0500

    first commit

---
+++ sportsdata_sdk/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from sportsdata_sdk.api.odds_api import OddsApi
+from sportsdata_sdk.api.scores_api import ScoresApi
